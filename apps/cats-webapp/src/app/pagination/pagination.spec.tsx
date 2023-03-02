7f5237d4d8f035b711ea5cc6401fc5e8920acf25
Lukas Raila <lukas.raila@zenitech.co.uk>
2023-03-02 14:36:11 +0200

    [T-03] Test pagination component

--- apps/cats-webapp/src/app/pagination/pagination.spec.tsx
+++ apps/cats-webapp/src/app/pagination/pagination.spec.tsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
 
 import Pagination from './pagination';
 
@@ -12,5 +13,50 @@ describe('Pagination', () => {
       />
     );
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should disable back button for first page', () => {
+    render(
+      <Pagination
+        activePage={1}
+        setActivePage={jest.fn()}
+        isNextButtonDisabled={false}
+      />
+    );
+    expect(screen.getByRole('button', { name: '<' })).toBeVisible();
+    expect(screen.getByRole('button', { name: '<' })).toBeDisabled();
+    expect(screen.getByRole('button', { name: '>' })).toBeVisible();
+    expect(screen.getByRole('button', { name: '>' })).toBeEnabled();
+    expect(screen.getByText(1)).toBeVisible();
+  });
+
+  it('should disable next button if "isNextButtonDisabled" is "true"', () => {
+    render(
+      <Pagination
+        activePage={1}
+        setActivePage={jest.fn()}
+        isNextButtonDisabled={true}
+      />
+    );
+    expect(screen.getByRole('button', { name: '<' })).toBeVisible();
+    expect(screen.getByRole('button', { name: '<' })).toBeDisabled();
+    expect(screen.getByRole('button', { name: '>' })).toBeVisible();
+    expect(screen.getByRole('button', { name: '>' })).toBeDisabled();
+    expect(screen.getByText(1)).toBeVisible();
+  });
+
+  it('should enable back button if "activePage" is "2"', () => {
+    render(
+      <Pagination
+        activePage={2}
+        setActivePage={jest.fn()}
+        isNextButtonDisabled={false}
+      />
+    );
+    expect(screen.getByRole('button', { name: '<' })).toBeVisible();
+    expect(screen.getByRole('button', { name: '<' })).toBeEnabled();
+    expect(screen.getByRole('button', { name: '>' })).toBeVisible();
+    expect(screen.getByRole('button', { name: '>' })).toBeEnabled();
+    expect(screen.getByText(2)).toBeVisible();
   });
 });
